0b3cef59453b1047541f1fa12b293a10e29afe78
Mikhail Shestakov <bond1982shestackov@yandex.ru>
2023-09-24 20:11:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.title("Стоимость автомобиля")
 
 year = st.number_input("Год выпуска", min_value=1900, max_value=datetime.now().year)
 
-maker = st.selectbox("Производитель", options=["jjjjjj", "gggg"], placeholder="Выберите вариант")
+maker = st.selectbox("Производитель", options=["jjjjjj", "gggg"], placeholder="Select contact method...")
 
 
 # In[15]:
